dd82493332e518d8f1f33f2cdf2656bf0478482c
lucasdd0 <lucasfo2005@gmail.com>
2026-02-28 22:05:35 -0300

    commit

--- ms-produto/src/main/java/br/fiap/com/ms/produto/dto/ProdutoDTO.java
+++ ms-produto/src/main/java/br/fiap/com/ms/produto/dto/ProdutoDTO.java
@@ -1,9 +1,13 @@
 package br.fiap.com.ms.produto.dto;
 
 import br.fiap.com.ms.produto.entities.Produto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.NotFound;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -11,8 +15,15 @@ import lombok.NoArgsConstructor;
 public class ProdutoDTO {
 
     private Long id;
+
+    @NotBlank(message = "Campo nome é requerido")
+    @Size(min = 3, max = 100, message = "O nome deve conter entre 3 e 100 caracteres")
     private String nome;
+    @NotBlank(message = "Campo descrição é requerido")
+    @Size(min = 10, message = "A descrição deve conter no minimo 10 caracteres")
     private String descricao;
+    @NotBlank(message = "Campo valor é requerido")
+    @Positive(message = "A campo valor não pode ser menor que 0")
     private Double valor;
 
     public ProdutoDTO(Produto produto) {
